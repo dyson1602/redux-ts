779286eae8fe3384ee2046fc6e5c1e28e6388e5a
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-06 20:53:10 -0400

    added: onSubmit function

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    //Don't forget to preventDefault on form submission so the page isn't
+    //constantly refreshed
+    event.preventDefault;
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
     </div>
