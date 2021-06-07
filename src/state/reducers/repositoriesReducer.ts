4ea0bbf1c12b1288b28fb81f11c16f7726a2116c
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-06 20:05:54 -0400

    added ActionType enum; applied enum to reducer

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -5,36 +5,45 @@ interface RepositoriesState {
 }
 
 interface SearchRepositoriesAction {
-  type: 'search_repositories';
+  type: ActionType.SEARCH_REPOSITORIES;
 }
 
 interface SearchRepositoriesSuccessAction {
-  type: 'search_repositories_success';
+  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
   payload: string[];
 }
 
 interface SearchRepositoriesErrorAction {
-  type: 'search_repositories_error';
+  type: ActionType.SEARCH_REPOSITORIES_ERROR;
   payload: string;
+}
+
+//Called a TypeUnion
+type Action =
+  | SearchRepositoriesAction
+  | SearchRepositoriesSuccessAction
+  | SearchRepositoriesErrorAction;
+
+enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
 }
 
 const reducer = (
   state: RepositoriesState,
-  action:
-    | SearchRepositoriesAction
-    | SearchRepositoriesSuccessAction
-    | SearchRepositoriesErrorAction
+  action: Action
 ): RepositoriesState => {
-    switch (action.type) {
-      case 'search_repositories':
-        return { loading: true, error: null, data: [] };
-      case 'search_repositories_success':
-        return { loading: false, error: null, data: action.payload };
-      case 'search_repositories_error':
-        return { loading: false, error: action.payload, data: [] };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
 };
 
 export default reducer;
